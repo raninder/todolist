13d3381626350690e326b7a0ee339a27ad262d0c
raninder dhillon <raninderdhillon@gmail.com>
2022-02-11 20:34:16 -0500

    graphQL API schema added

--- react/amplify-react-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ react/amplify-react-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,11 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "amplifyreactapp": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
     }
 }
--- react/amplify-react-app/src/App.js
+++ react/amplify-react-app/src/App.js
@@ -1,15 +1,34 @@
 // works for ui-react 2.0.0
 // check https://ui.docs.amplify.aws/
 
-import { Amplify } from 'aws-amplify';
 
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import awsExports from './aws-exports';
+import React, { useState, useEffect } from 'react';
+import { listSongs } from './graphql/queries';
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
 Amplify.configure(awsExports);
 
 export default function App() {
+	const [songs, setSongs] = useState([]);
+
+const fetchSongs = async () => {
+    try {
+        const songData = await API.graphql(graphqlOperation(listSongs));
+        const songList = songData.data.listSongs.items;
+        console.log('song list', songList);
+        setSongs(songList);
+    } catch (error) {
+        console.log('error on fetching songs', error);
+    }
+};
+
+useEffect(() => {
+	fetchSongs();
+}, []);
+
   return (
     <Authenticator>
       {({ signOut, user }) => (
